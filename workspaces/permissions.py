3917dca0c98e45cab345d978bc0dd2707cbe82e4
snarayanank2 <siva@fyle.in>
2020-05-18 09:32:06 +0530

    Reduce logging

--- workspaces/permissions.py
+++ workspaces/permissions.py
@@ -10,12 +10,12 @@ class Permission(BasePermission):
 
     def has_permission(self, request, view):
         principal = AuthUtils.get_current_principal()
-        logger.info('has_permission called for principal %s', principal)
-        logger.info('info on view %s', inspect.getmembers(view))
-        logger.info('info on request path: %s, data: %s', request.get_full_path(), request.data)
+        # logger.info('has_permission called for principal %s', principal)
+        # logger.info('info on view %s', inspect.getmembers(view))
+        # logger.info('info on request path: %s, data: %s', request.get_full_path(), request.data)
         return True
 
     def has_object_permission(self, request, view, obj):
         principal = AuthUtils.get_current_principal()
-        logger.info('has_object_permission called %s', principal)
+        # logger.info('has_object_permission called %s', principal)
         return True
