d8287c495bf5a079c5def09114d7c342f76e5d6a
snarayanank2 <siva@fyle.in>
2020-05-28 13:48:53 +0530

    Change back to individual

--- workspaces/urls.py
+++ workspaces/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('workspaces.common.urls')),
+    path('individual/', include('workspaces.individual.urls')),
     path('admin/', include('workspaces.admin.urls')),
 ]
