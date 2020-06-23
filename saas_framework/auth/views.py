5182e10415481bb34249dc9e66f70e594b9a4111
snarayanank2 <siva@fyle.in>
2020-06-23 22:17:31 +0530

    Moved all token related code to common

--- saas_framework/auth/views.py
+++ saas_framework/auth/views.py
@@ -1,110 +1,72 @@
 import logging
 
-from django.contrib.auth import authenticate
-from django.contrib.auth.hashers import make_password
-from django.contrib.auth.models import User
-from django.http import HttpResponse
-from django.http.response import HttpResponseForbidden
-from rest_framework import viewsets
-from rest_framework.decorators import action
 from rest_framework.exceptions import AuthenticationFailed, PermissionDenied
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from saas_framework.core.accounts.models import Account
-from saas_framework.core.principals.models import Principal
-from saas_framework.core.tpas.models import ThirdPartyApp
-from saas_framework.core.tpas.views import OAuth2Authorize, OAuth2Token
-from saas_framework.core.workspaces.models import Workspace
-from saas_framework.core.auth.claim import Claim
+from saas_framework.core.auth.token import TokenUtils
 
 logger = logging.getLogger(__name__)
 
 class BasicAuthSigninView(APIView):
     def post(self, request):
-        """
-        Should return access_token, refresh_token
-        """
         email = request.data.get('email', None)
         password = request.data.get('password', None)
-        user = authenticate(username=email, password=password)
-        if user is None:
-            raise AuthenticationFailed()
         app_name = request.data.get('app_name', 'webapp')
-        tpa = ThirdPartyApp.objects.get(name=app_name)
-        account = Account.objects.filter(user=user).order_by('-created_at').all()[0]
-        # log into the oldest workspace by default
-        (principal, created) = Principal.objects.get_or_create(account=account, tpa=tpa, roles=account.roles)
-        claim = Claim(user_id=user.id, workspace_id=account.workspace.id, principal_id=principal.id, tpa_id=tpa.id, account_id=account.id, roles=account.roles)
-        refresh_token = claim.to_token(exp_seconds=7*24*3600)
-        access_token = claim.to_token(exp_seconds=600)
+        (refresh_token, access_token) = TokenUtils.signin(email=email, password=password, app_name=app_name)
         return Response({ 'refresh_token': refresh_token, 'access_token': access_token })
 
 class BasicAuthSignupView(APIView):
     def post(self, request):
-        """
-        Should return access_token, refresh_token
-        """
         email = request.data.get('email', None)
         first_name = request.data.get('first_name', None)
         last_name = request.data.get('last_name', None)
-        password = request.data.get('password', None) # should confirm password come here?
-        if User.objects.filter(username=email).exists():
-            raise PermissionDenied()
-        user = User.objects.create(first_name=first_name, last_name=last_name, email=email, username=email, password=make_password(password))
+        password = request.data.get('password', None) # TODO: should confirm password come here?
         app_name = request.data.get('app_name', 'webapp')
-        tpa = ThirdPartyApp.objects.get(name=app_name)
-        workspace = Workspace.objects.create(name='Default')
-        account = Account.objects.create(user=user, workspace=workspace, roles='admin')
-        (principal, created) = Principal.objects.get_or_create(workspace=account.workspace, user=account.user, tpa=tpa)
-        claim = Claim(user_id=user.id, workspace_id=workspace.id, principal_id=principal.id, tpa_id=tpa.id, account_id=account.id, roles=account.roles)
-        refresh_token = claim.to_token(exp_seconds=7*24*3600)
-        access_token = claim.to_token(exp_seconds=600)
+        (refresh_token, access_token) = TokenUtils.signup(email=email, first_name=first_name, last_name=last_name, password=password, app_name=app_name)
         return Response({ 'refresh_token': refresh_token, 'access_token': access_token })
 
 class RefreshTokenView(APIView):
     def post(self, request):
-        """
-        Should return access_token
-        """
-        claim = Claim.from_token(token=request.data.get('refresh_token', None))
-        if not claim.user_id:
-            raise PermissionDenied()
-
-        # TODO: should check if account is disabled
-        access_token = claim.to_token(exp_seconds=600)
+        refresh_token = request.data.get('refresh_token', None)
+        access_token = TokenUtils.access_token(refresh_token=refresh_token)
         return Response({ 'access_token' : access_token })
 
 class SwitchWorkspaceView(APIView):
     def post(self, request):
         refresh_token = request.data.get('refresh_token')
         workspace_id = request.data.get('workspace_id')
-        claim = Claim.from_token(token=refresh_token)
-        if not claim.user_id:
-            raise PermissionDenied()
-        principal = Principal.objects.get(id=claim.principal_id)
-        workspace = Workspace.objects.get(id=workspace_id)
-        account = Account.objects.get(workspace=workspace, user=principal.account.user)
-        (principal, created) = Principal.objects.get_or_create(account=account, tpa=ThirdPartyApp.objects.get(id=claim.tpa_id), roles=account.roles)
-        claim1 = Claim(principal_id=principal.id, user_id=principal.account.user.id, account_id=principal.account.id, workspace_id=principal.account.workspace.id, roles=principal.roles, tpa_id=principal.tpa.id)
-        refresh_token = claim1.to_token(exp_seconds=7*24*3600)
-        access_token = claim1.to_token(exp_seconds=600)
+        (refresh_token, access_token) = TokenUtils.switch_workspace(refresh_token=refresh_token, workspace_id=workspace_id)
         return Response({ 'refresh_token': refresh_token, 'access_token': access_token })
-
-class OAuth2Authorize(OAuth2Authorize):
-    def get(self, request):
-        logger.info('request_params %s', request.query_params)
-        request.query_params._mutable = True
-        request.query_params['workspace_id'] = request.claim.workspace_id
-        request.query_params['account_id'] = request.claim.account_id
-        request.query_params._mutable = False
-        return super().get(request)
-
-class OAuthToken(OAuth2Authorize):
-    pass
 
 class JwtDecode(APIView):
     def post(self, request):
         jwt = request.data['jwt']
         claim = Claim.from_token(token=jwt)
         return Response({ 'claim' : claim.__dict__ })
+
+class OAuth2Authorize(APIView):
+    def get(self, request):
+        claim = request.claim
+        client_id = request.query_params.get('client_id')
+        response_type = request.query_params.get('response_type')
+        assert response_type in ['code']
+        state = request.query_params.get('state', '')
+        redirect_uri = request.query_params.get('redirect_uri')
+        scope = request.query_params.get('scope')
+        code = TokenUtils.oauth2_code(claim=claim, client_id=client_id, scope=scope)
+        redirect_uri = redirect_uri + f'?code={code}&state={state}'
+        return Response({ 'redirect_uri' : redirect_uri })
+
+class OAuth2Token(APIView):
+    def post(self, request):
+        client_id = request.data.get('client_id')
+        client_secret = request.data.get('client_secret')
+        grant_type = request.data.get('grant_type')
+        assert grant_type in ['authorization_code', 'refresh_token']
+        if grant_type == 'authorization_code':
+            code = request.data.get('code')
+            (refresh_token, access_token) = TupleUtils.oauth2_refresh_token(client_id=client_id, client_secret=client_secret, code=code)
+        else:
+            (refresh_token, access_token) = TupleUtils.oauth2_access_token(client_id=client_id, client_secret=client_secret, refresh_token=refresh_token)
+        return Response({ 'refresh_token' : refresh_token, 'access_token' : access_token })
---
+++ saas_framework/core/auth/token.py
@@ -0,0 +1,114 @@
+from typing import Tuple
+import logging
+from django.contrib.auth import authenticate
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import User
+from rest_framework.exceptions import AuthenticationFailed, PermissionDenied
+
+from saas_framework.core.accounts.models import Account
+from saas_framework.core.principals.models import Principal
+from saas_framework.core.tpas.models import ThirdPartyApp
+from saas_framework.core.workspaces.models import Workspace
+from saas_framework.core.auth.claim import Claim
+
+logger = logging.getLogger(__name__)
+
+class TokenUtils:
+
+    @staticmethod
+    def signin(email, password, app_name) -> Tuple[str, str]:
+        user = authenticate(username=email, password=password)
+        if user is None:
+            raise AuthenticationFailed()
+        tpa = ThirdPartyApp.objects.get(name=app_name)
+        account = Account.objects.filter(user=user).order_by('id').all()[0]
+        # log into the oldest workspace by default
+        (principal, created) = Principal.objects.get_or_create(account=account, tpa=tpa, roles=account.roles)
+        claim = Claim(user_id=user.id, workspace_id=account.workspace.id, principal_id=principal.id, tpa_id=tpa.id, account_id=account.id, roles=account.roles)
+        refresh_token = claim.to_token(exp_seconds=7*24*3600)
+        access_token = claim.to_token(exp_seconds=600)
+        return (refresh_token, access_token)
+
+    @staticmethod
+    def signup(email, first_name, last_name, password, app_name) -> Tuple[str, str]:
+        if User.objects.filter(username=email).exists():
+            raise PermissionDenied()
+        user = User.objects.create(first_name=first_name, last_name=last_name, email=email, username=email, password=make_password(password))
+        tpa = ThirdPartyApp.objects.get(name=app_name)
+        workspace = Workspace.objects.create(name='Default')
+        account = Account.objects.create(user=user, workspace=workspace, roles='common,admin')
+        (principal, created) = Principal.objects.get_or_create(workspace=account.workspace, user=account.user, tpa=tpa)
+        claim = Claim(user_id=user.id, workspace_id=workspace.id, principal_id=principal.id, tpa_id=tpa.id, account_id=account.id, roles=account.roles)
+        refresh_token = claim.to_token(exp_seconds=7*24*3600)
+        access_token = claim.to_token(exp_seconds=600)
+        return (refresh_token, access_token)
+
+    @staticmethod
+    def access_token(refresh_token) -> str:
+        claim = Claim.from_token(token=refresh_token)
+        if not claim.user_id:
+            raise PermissionDenied()
+
+        # TODO: should check if account is disabled
+        access_token = claim.to_token(exp_seconds=600)
+        return access_token
+
+    @staticmethod
+    def switch_workspace(refresh_token, workspace_id) -> Tuple[str, str]:
+        claim = Claim.from_token(token=refresh_token)
+        if not claim.user_id:
+            raise PermissionDenied()
+        principal = Principal.objects.get(id=claim.principal_id)
+        workspace = Workspace.objects.get(id=workspace_id)
+        account = Account.objects.get(workspace=workspace, user=principal.account.user)
+        (principal, created) = Principal.objects.get_or_create(account=account, tpa=ThirdPartyApp.objects.get(id=claim.tpa_id), roles=account.roles)
+        claim1 = Claim(principal_id=principal.id, user_id=principal.account.user.id, account_id=principal.account.id, workspace_id=principal.account.workspace.id, roles=principal.roles, tpa_id=principal.tpa.id)
+        refresh_token = claim1.to_token(exp_seconds=7*24*3600)
+        access_token = claim1.to_token(exp_seconds=600)
+        return (refresh_token, access_token)
+
+    @staticmethod
+    def oauth2_code(claim, client_id, scope) -> str:
+        if not claim.user_id:
+            raise PermissionDenied()
+        workspace = Workspace.objects.get(id=claim.workspace_id)
+        account = Account.objects.get(id=claim.account_id)
+        tpa = ThirdPartyApp.objects.get(id=client_id)
+        extra_roles = set(scope.split(',')) - set(account.roles.split(','))
+        if len(extra_roles) > 0:
+            raise PermissionDenied()
+        roles = scope
+        atpa = AccountThirdPartyApp.objects.create(workspace=workspace, tpa=tpa, account=account, roles=roles)
+        claim1 = Claim(atpa_id=atpa.id)
+        code = claim1.to_token(exp_seconds=300)
+        return code
+
+    @staticmethod
+    def oauth2_refresh_token(client_id, client_secret, code) -> Tuple[str, str]:
+        tpa = ThirdPartyApp.objects.get(id=client_id)
+        if tpa.secret != client_secret:
+            raise PermissionDenied()
+        claim = Claim.from_token(token=code)
+        atpa_id = claim.atpa_id
+        atpa = AccountThirdPartyApp.objects.get(id=atpa_id)
+        assert tpa == atpa.tpa
+        (principal, created) = Principal.objects.get_or_create(account=atpa.account, tpa=atpa.tpa, roles=atpa.roles)
+        claim1 = Claim(user_id=atpa.account.user.id, workspace_id=atpa.account.workspace.id, tpa_id=atpa.tpa.id, account_id=atpa.account.id, principal_id=principal.id, roles=atpa.roles)
+        refresh_token = claim1.to_token(exp_seconds=30*24*3600) # consider 10 years
+        access_token = claim1.to_token(exp_seconds=3600)
+        return (refresh_token, access_token)
+
+    @staticmethod
+    def oauth2_access_token(client_id, client_secret, refresh_token) -> Tuple[str, str]:
+        tpa = ThirdPartyApp.objects.get(id=client_id)
+        if tpa.secret != make_password(client_secret):
+            raise PermissionDenied()
+        claim = Claim.from_token(token=refresh_token)
+        if not claim.user_id:
+            raise PermissionDenied()
+        principal_id = claim.principal_id
+        principal = Principal.objects.get(id=principal_id)
+        assert principal.tpa == tpa
+        claim1 = Claim(principal_id=principal.id, user_id=principal.user.id, workspace_id=principal.account.workspace.id, account_id=principal.account.id, tpa_id=principal.tpa.id, roles=principal.roles)
+        access_token = claim1.to_token(exp_seconds=600)
+        return (refresh_token, access_token)
