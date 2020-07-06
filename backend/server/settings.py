954b31393bcf53078a013762e00695b6b2ac04cf
snarayanank2 <siva@fyle.in>
2020-07-06 21:56:08 +0530

    Use python 3.8 and update settings

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -73,8 +73,8 @@ WSGI_APPLICATION = 'server.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db_prod.sqlite3'),
-        'TEST': {'NAME': os.path.join(BASE_DIR, 'db_test.sqlite3')}
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'TEST': {'NAME': os.path.join(BASE_DIR, 'db.sqlite3')}
     },
     # 'default': {
     #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
